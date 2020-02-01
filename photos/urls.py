731bab6745221c0a8717c48aa398411a8783e73a
Greg <blackrocket.47@gmail.com>
2020-02-01 11:25:05 +0300

    Added app name photos in the app

--- photos/urls.py
+++ photos/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+app_name ='photos'
+
+
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
 ]
