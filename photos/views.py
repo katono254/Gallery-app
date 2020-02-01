680ae9314eec90664b61da2fa056eb65e7314fb1
Greg <blackrocket.47@gmail.com>
2020-02-01 12:51:27 +0300

    Added files to the template section of the app

--- photos/views.py
+++ photos/views.py
@@ -6,6 +6,11 @@ def welcome(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     print(locations)
-    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
+    return render(request, 'Render/index.html', {'images': images[::-1], 'locations': locations})
 
 
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'Render/location.html', {'location_images': images})
