299e12df09a1eb7b7f5fc6e110af473bcba0c6aa
Greg <blackrocket.47@gmail.com>
2020-02-04 10:36:05 +0300

    Added heroku master to the app

--- gallery/settings.py
+++ gallery/settings.py
@@ -14,7 +14,10 @@ import os
 from decouple import config, Csv
 import dj_database_url
 import django_heroku
-from decouple import config,Csv
+
+
+
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
